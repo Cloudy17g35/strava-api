7732a4c1ce5dc9cc97f8cc550dcebfe24afab472
Ksawery Lejczak <ksawery.lejczak@gmail.com>
2023-09-12 22:13:37 +0200

    adding data prep

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from src.api_methods import get_methods
 from src.api_methods import authorize
+from src.data_preprocessing import main as data_prep
+
 
 def main():
     token:str = authorize.get_acces_token()
-    resp:dict = get_methods.access_activity_data(token)
+    data:dict = get_methods.access_activity_data(token)
+    df = data_prep.preprocess_data(data)
+    df.to_csv('otp.csv', index=False)
     
 
 
