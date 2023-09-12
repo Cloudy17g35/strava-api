c6483c9af664adf87268506c4dbc281808170e17
Ksawery Lejczak <ksawery.lejczak@gmail.com>
2023-09-12 23:32:32 +0200

    removing data preprocessing part/ adding datetime.now as a file suffix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from pathlib import Path
+
 
 from src.api_methods import get_methods
 from src.api_methods import authorize
@@ -9,7 +11,8 @@ def main():
     token:str = authorize.get_acces_token()
     data:dict = get_methods.access_activity_data(token)
     df = data_prep.preprocess_data(data)
-    df.to_csv(Path('data', 'my_activity_data.csv'), index=False)
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    df.to_csv(Path('data', f'my_activity_data={timestamp}.csv'), index=False)
 
 
 if __name__ == '__main__':
--- src/data_preprocessing/main.py
+++ src/data_preprocessing/main.py
@@ -1,47 +1,7 @@
 import pandas as pd
 
 
-def convert_m_per_s_to_km_per_h(meters_per_sec:float) -> float:
-    # Conversion factor
-    conversion_factor = 3.6
-    
-    # Perform the conversion
-    km_per_h = meters_per_sec * conversion_factor
-    
-    return round(km_per_h, 2)
-    
+def preprocess_data(data:dict) -> pd.DataFrame:
 
-def preprocess_data(data:dict):
-
-    df = pd.json_normalize(data)
-    
-    columns_selected:list[str] = [
-        "name",
-        "has_heartrate",
-        "type",
-        "sport_type",
-        "start_date_local",
-        "moving_time",
-        "elapsed_time",
-        "distance",
-        "total_elevation_gain",
-        "achievement_count",
-        "average_speed",
-        "max_speed",
-        "average_heartrate",
-        "max_heartrate"
-    ]
-    
-    df = df[columns_selected]
-    df[["average_speed", "max_speed",]] = df[["average_speed", "max_speed",]].apply(convert_m_per_s_to_km_per_h)
-    columns_mapper:dict = {
-        "average_speed": "average_speed (km/h)",
-        "max_speed": "max_speed (km/h)",
-        "moving_time" : "moving_time (s)",
-        "elapsed_time": "elapsed_time (s)",
-        "distance" : "distance (m)",
-        "total_elevation_gain": "total_elevation_gain (m)",
-    }
-    df.rename(columns=columns_mapper, inplace=True)
-    return df
+    return pd.json_normalize(data)
 
