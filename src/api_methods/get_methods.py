0c7745f60ed910a3ec288ce40f10d00b4d474c87
Ksawery Lejczak <ksawery.lejczak@gmail.com>
2024-02-17 18:56:06 +0100

    add params to GET activities request/increase activities number

--- src/api_methods/get_methods.py
+++ src/api_methods/get_methods.py
@@ -3,7 +3,11 @@ import requests
 from src.api_methods import endpoints
 
 
-def access_activity_data(access_token:str) -> dict:
+def access_activity_data(access_token:str, params:dict=None) -> dict:
     headers:dict = {'Authorization': f'Authorization: Bearer {access_token}'}
-    my_dataset:dict = requests.get(endpoints.activites_endpoint, headers=headers).json()
-    return my_dataset
+    if not params:
+        response:dict = requests.get(endpoints.activites_endpoint, headers=headers)
+    response:dict = requests.get(endpoints.activites_endpoint, headers=headers, params=params)
+    response.raise_for_status()
+    activity_data = response.json()
+    return activity_data
