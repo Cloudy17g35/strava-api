047c7601a38ae2e0b48ebc38a3bddb1dcf87efe1
Ksawery Lejczak <ksawery.lejczak@gmail.com>
2024-10-24 16:59:12 +0200

    change reqs/ fetch ALL the activities data from all the pages

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from datetime import datetime
 from pathlib import Path
-
+import pandas as pd
 
 from src.api_methods import get_methods
 from src.api_methods import authorize
 from src.data_preprocessing import main as data_prep
 
-# used to f.e set the limit of fetched activities (default - 30)
-ACTIVITIES_PER_PAGE = 200
-# current page number with activities
-PAGE_NUMBER = 1
-
-
-GET_ALL_ACTIVITIES_PARAMS = {
-    'per_page': ACTIVITIES_PER_PAGE,
-    'page': PAGE_NUMBER
-}
-
 
 def main():
     token:str = authorize.get_acces_token()
-    data:dict = get_methods.access_activity_data(token, params=GET_ALL_ACTIVITIES_PARAMS)
-    df = data_prep.preprocess_data(data)
+    dfs_to_concat = []
+    page_number = 1
+    while True:
+        data:dict = get_methods.access_activity_data(token, params={
+            'per_page': 200,
+            'page': page_number,
+        })
+        page_number += 1
+        cur_df = data_prep.preprocess_data(data)
+        dfs_to_concat.append(cur_df)
+        if len(data) == 0:
+            break
+    
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    df = pd.concat(dfs_to_concat, ignore_index=True)
     df.to_csv(Path('data', f'my_activity_data={timestamp}.csv'), index=False)
 
 
